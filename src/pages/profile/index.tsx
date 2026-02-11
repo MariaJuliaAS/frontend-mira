86639181d0ff21fc26ba30c9b1b60e852692548c
MariaJulia <mariajuliadev@gmail.com>
2026-02-11 16:24:30 -0300

    feat: Calling and consuming user statistics

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -3,8 +3,7 @@ import { Container } from "../../components/container";
 import { useEffect, useState } from "react";
 import { api } from "../../service/api";
 import { FaRegBuilding, FaRegUser } from "react-icons/fa";
-import { MdOutlineEmail } from "react-icons/md";
-import { LuBookOpen, LuCalendar } from "react-icons/lu";
+import { LuBookOpen, LuCalendar, LuClock, LuTarget } from "react-icons/lu";
 
 interface UserDetailProps {
     id: string;
@@ -17,8 +16,17 @@ interface UserDetailProps {
     }
 }
 
+interface UserStatsProps {
+    totalStudyTime: {
+        time: number
+    };
+    activeGoals: number;
+    activeCourses: number;
+}
+
 export function Profile() {
     const [userDetail, setUserDetail] = useState<UserDetailProps>();
+    const [userStats, setUserStats] = useState<UserStatsProps>();
 
     async function fetchUserDetail() {
         const token = localStorage.getItem("@tokenMira");
@@ -40,9 +48,42 @@ export function Profile() {
         }
     }
 
+    async function fetchUserStats() {
+        const token = localStorage.getItem("@tokenMira");
+
+        if (!token) {
+            console.error("No token found");
+            return;
+        }
+
+        try {
+            const response = await api.get("/user/stats", {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            })
+            setUserStats(response.data);
+        } catch (err) {
+            console.error("Erro ao buscar estatísticas do usuário: ", err)
+        }
+    }
+
     useEffect(() => {
         fetchUserDetail()
+        fetchUserStats()
     }, [])
+
+    function secondsToHours(totalSeconds: number | undefined) {
+        if (totalSeconds === undefined) {
+            return "0"
+        }
+
+        const hours = Math.floor(totalSeconds / 3600);
+        const minutes = Math.floor((totalSeconds % 3600) / 60);
+        const seconds = totalSeconds % 60;
+
+        return `${hours}h${minutes}m${seconds}s`;
+    }
 
     return (
         <main className="bg-zinc-200/10 min-h-screen">
@@ -56,7 +97,7 @@ export function Profile() {
                     </div>
                 </header>
 
-                <div className="mt-8 sm:mt-12 flex gap-6 lg:gap-8">
+                <div className="mt-8 sm:mt-12 flex gap-6 lg:gap-8 min-h-full">
                     <section className="w-full flex-2 bg-white border border-gray-200 rounded-2xl p-4 shadow-lg">
                         <div className="flex w-full justify-between">
                             <h2 className="text-lg font-semibold capitalize flex items-center gap-2">Informações do Usuário</h2>
@@ -97,9 +138,36 @@ export function Profile() {
                             </div>
                         </div>
                     </section>
-                    <section className="w-full flex-1 bg-white border border-gray-200 rounded-2xl p-4 shadow-lg">
-                        <p>s2</p>
+                    <section className="w-full flex-1 ">
+                        <div className="flex flex-col gap-4 h-full">
+
+                            <div className="flex-1 bg-white border border-gray-200 rounded-2xl p-4 shadow-lg flex flex-col items-center justify-center">
+                                <span className="flex h-10 w-10 items-center justify-center rounded-xl bg-blue-100 text-blue-600 font-semibold">
+                                    <LuClock size={24} className="text-blue-950" />
+                                </span>
+                                <p className="font-bold text-xl mt-2">{secondsToHours(userStats?.totalStudyTime.time)}</p>
+                                <span className="text-zinc-500 text-sm">Horas estudadas</span>
+                            </div>
+
+                            <div className="flex-1 bg-white border border-gray-200 rounded-2xl p-4 shadow-lg flex flex-col items-center justify-center">
+                                <span className="flex h-10 w-10 items-center justify-center rounded-xl bg-blue-100 text-blue-600 font-semibold">
+                                    <LuBookOpen size={24} className="text-blue-950" />
+                                </span>
+                                <p className="font-bold text-xl mt-2">{userStats?.activeCourses}</p>
+                                <span className="text-zinc-500 text-sm">Matérias ativas</span>
+                            </div>
+
+                            <div className="flex-1 bg-white border border-gray-200 rounded-2xl p-4 shadow-lg flex flex-col items-center justify-center">
+                                <span className="flex h-10 w-10 items-center justify-center rounded-xl bg-blue-100 text-blue-600 font-semibold">
+                                    <LuTarget size={24} className="text-blue-950" />
+                                </span>
+                                <p className="font-bold text-xl mt-2">{userStats?.activeGoals}</p>
+                                <span className="text-zinc-500 text-sm">Metas ativas</span>
+                            </div>
+
+                        </div>
                     </section>
+
                 </div>
             </Container>
         </main>
